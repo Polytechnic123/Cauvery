f1d5c17404aff8b388d120588c18b50d960c6aa8
Harshitha <daydarker28@gmail.com>
2023-06-01 11:03:15 +0530

    1st June

---
+++ add.py
@@ -0,0 +1,8 @@
+a=int(input("enter a value "))
+b=int(input("enter b value "))
+sum=a+b
+print("enter a&b value ",sum)
+s1=input("enter s1 value ")
+s2=input("enter s2 value ")
+s=s1+s2
+print("sum of s1&s2 ",s)
